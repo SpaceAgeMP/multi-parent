7ac18a026556fd7f74613ff51404a75af5236420
Mark Dietzer <git@doridian.net>
2021-02-15 17:18:28 -0800

    rm

--- lua/autorun/server/parentfix.lua
+++ lua/autorun/server/parentfix.lua
@@ -1,11 +1,5 @@
 local ENT = FindMetaTable("Entity")
 
-local PARENT_CONSTRAINT = "sb_ent_parent"
-
-duplicator.RegisterEntityModifier(PARENT_CONSTRAINT, function (ply, ent, data)
-    print("PP", ent, data.parent)
-end)
-
 if not ENT.RealSetParent then
     ENT.RealSetParent = ENT.SetParent
 end
@@ -14,6 +8,5 @@ function ENT:SetParent(other)
     if self:IsVehicle() then
         return
     end
-    duplicator.StoreEntityModifier(self, PARENT_CONSTRAINT, { parent = other })
     self:RealSetParent(other)
 end
